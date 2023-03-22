56beb1a0d167c899a80836fb830a0741123a4045
Ahmed Abdullah <ahmedoffical001@gmail.com>
2023-03-22 16:19:51 +0400

    update1

--- subjects.js
+++ subjects.js
@@ -1,61 +1,64 @@
 let subjects = [{
-        "id": 101,
-        // "image": "img/FlyDubai.png",
-        "subject": 'Laser Pointer',
-        "location": 'Sweden',
-        "price": 220,
-        "Spaces": 'this is a Laser pointer from Sweden',
-        // "availableInventory": 8,
-        // "rating": 2
-    }
 
-    // {
+    'lesson': "Art",
+    'location': 'sweden',
+    'price': 450,
+    'spaces': 5,
+    'image': "img/art.jpg"
+}, {
+    'lesson': "Cooking",
+    'location': 'Dubai',
+    'price': 400,
+    'spaces': 5,
+    'image': "img/cooking.png"
+}, {
+    'lesson': "English",
+    'location': 'Sweden ',
+    'price': 300,
+    'spaces': 5,
+    'image': "img/english.jpg"
+}, {
+    'lesson': "Football",
+    'location': 'London ',
+    'price': 480,
+    'spaces': 5,
+    'image': "img/football.jpg"
+}, {
+    'lesson': "French",
+    'location': 'Sweden ',
+    'price': 380,
+    'spaces': 5,
+    'image': "img/french.jpg"
+}, {
+    'lesson': "German",
+    'location': 'Dubai ',
+    'price': 350,
+    'spaces': 5,
+    'image': "img/german.jpg"
+}, {
+    'lesson': "Math",
+    'location': 'London ',
+    'price': 300,
+    'spaces': 5,
+    'image': "img/math symbol.png"
+}, {
+    'lesson': "Swimming",
+    'location': 'London',
+    'price': 500,
+    'spaces': 5,
+    'image': "img/swimming.jpg"
+}, {
+    'lesson': "Tailoring",
+    'location': 'America ',
+    'price': 450,
+    'spaces': 5,
+    'image': "img/tailoring.jpg"
 
-    //     "id": 202,
-    //     "image": 'images/math symbol.png',
-    //     "subject": 'blue pointer',
-    //     "location": 'Sweden',
-    //     "price": 220,
-    //     "detail": 'this is a blue pointer from Sweden',
-    //     "availableInventory": 8,
-    //     "rating": 3
-    // },
+}, {
+    'lesson': "Robotics",
+    'location': 'America ',
+    'price': 300,
+    'spaces': 5,
+    'image': "img/robot.png"
 
-    // {
-
-    //     "id": 303,
-    //     "image": 'images/math symbol.png',
-    //     "subject": 'red pointer',
-    //     "location": 'Sweden',
-    //     "price": 220,
-    //     "detail": 'this is a red pointer from Sweden',
-    //     "availableInventory": 8,
-    //     "rating": 4
-    // },
-
-    // {
-
-    //     "id": '404',
-    //     "image": 'images/math symbol.png',
-    //     "subject": 'yellow pointer',
-    //     "location": 'Sweden',
-    //     "price": 220,
-    //     "detail": 'this is a yellow pointer from Sweden',
-    //     "availableInventory": 8,
-    //     "rating": 2
-    // },
-
-    // {
-
-    //     "id": 505,
-    //     "image": 'images/math symbol.png',
-    //     "subject": 'black pointer',
-    //     "location": 'Sweden',
-    //     "price": 220,
-    //     "detail": 'this is a black pointer from Sweden',
-    //     "availableInventory": 8,
-    //     "rating": 5
-    // }
-
-
-]
+}, ],
